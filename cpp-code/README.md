a97f5a5b152d3350586e5d85ac9cf4c2b602a988
Daniel Nilsson <tfy09dnn@cs.umu.se>
2017-04-21 11:18:46 +0200

    folder structure c++

--- cpp-code/README.md
+++ cpp-code/README.md
@@ -1,5 +1,26 @@
 # C++
-Mosaic Pipeline:
+
+## src
+source for the application
+
+## tests
+test code for the application
+
+## lib
+libraries
+
+## include 
+interface for the library *.h
+
+## doc
+documentation of any kind
+
+## spikes
+small tests for proof-of-concept
+
+
+
+# Mosaic Pipeline
 Geotag->Image compression->Find neighbours->SIFT->RANSAC->Bundle Adjustment->Mosaic
 
 ## Geotag
---
+++ cpp-code/src/TreeMarkupToolbox.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : TreeMarkupToolbox.cpp
+// Author      : MV
+// Version     :
+// Copyright   : GNU
+// Description : Method for positioning field measured trees relative to image data for building a 3D model.
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Program start");
+
+	puts("Program end");
+	return EXIT_SUCCESS;
+}
