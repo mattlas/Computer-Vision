aa1bda8118d5f47ec8d9b8e7ae94c8267dbf42f4
Tino Schulz <tinu@Tinos-MacBook-Pro.local>
2017-04-24 11:30:02 +0200

    load and display image

--- Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
+++ Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.treemapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SubsamplingScaleImageView imageView = (SubsamplingScaleImageView) findViewById(R.id.imageView);
+
+        imageView.setImage(ImageSource.resource(R.drawable.tree));
+        //imageView.setImage(ImageSource.uri("/sdcard/DCIM/DSCM00123.JPG"));
+
+
     }
+
 }
