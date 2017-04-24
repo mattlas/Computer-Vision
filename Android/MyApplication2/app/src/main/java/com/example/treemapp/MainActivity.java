ef19f9c754d49a49d4154a9cf38cfede41e43079
Tino Schulz <tinu@Tinos-MacBook-Pro.local>
2017-04-24 14:15:24 +0200

    event handling

--- Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
+++ Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.treemapp;
 
+import android.graphics.Canvas;
 import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
+import static android.R.attr.id;
 import static com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView.ORIENTATION_0;
 
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity  /*, OnLongClickListener*/ {
 
     private SubsamplingScaleImageView imageView;
 
@@ -28,26 +31,60 @@ public class MainActivity extends AppCompatActivity {
         imageView.setImage(ImageSource.resource(R.drawable.tree));
         //imageView.setImage(ImageSource.uri("/sdcard/DCIM/DSCM00123.JPG"));
 
+        initialiseEventHandling();
+
         // Display image in its native orientation
         imageView.setOrientation(ORIENTATION_0);
-
-        final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e) {
-                Toast error = Toast.makeText(MainActivity.this, "Error 1!", Toast.LENGTH_SHORT);
-                error.show();
-
-                if (imageView.isReady()) {
-                    PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
-                    // ...
-
-                    Toast errorToast = Toast.makeText(MainActivity.this, "Error, pls chech your internet connection and try again!", Toast.LENGTH_SHORT);
-                    errorToast.show();
-                }
-                return true;
-            }
-        });
     }
 
 
+
+    private void initialiseEventHandling() {
+        final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent e) {
+                if (imageView.isReady()) {
+                    PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                    Toast.makeText(getApplicationContext(), "Single tap: " + ((int)sCoord.x) + ", " + ((int)sCoord.y), Toast.LENGTH_SHORT).show();
+
+                    // Mark tree
+
+
+                    // Pop up menu
+
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Single tap: Image not ready", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+            @Override
+            public void onLongPress(MotionEvent e) {
+                if (imageView.isReady()) {
+                    PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                    Toast.makeText(getApplicationContext(), "Long press: " + ((int)sCoord.x) + ", " + ((int)sCoord.y), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Long press: Image not ready", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                return super.onDoubleTap(e);
+                /*if (imageView.isReady()) {
+                    PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                    Toast.makeText(getApplicationContext(), "Double tap: " + ((int)sCoord.x) + ", " + ((int)sCoord.y), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Double tap: Image not ready", Toast.LENGTH_SHORT).show();
+                }
+                return true;*/
+            }
+        });
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return gestureDetector.onTouchEvent(motionEvent);
+            }
+        });
+    }
 }
