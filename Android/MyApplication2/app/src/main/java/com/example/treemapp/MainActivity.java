bdcabed2f4567c4a330cf0697bb3cf9ff2c1ef71
Tino Schulz <tinu@Tinos-MacBook-Pro.local>
2017-04-24 11:35:58 +0200

    cleaned up main activity code

--- Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
+++ Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
@@ -1,18 +1,13 @@
 package com.example.treemapp;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
-import java.io.File;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,12 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setting the image to display
         SubsamplingScaleImageView imageView = (SubsamplingScaleImageView) findViewById(R.id.imageView);
-
         imageView.setImage(ImageSource.resource(R.drawable.tree));
         //imageView.setImage(ImageSource.uri("/sdcard/DCIM/DSCM00123.JPG"));
-
-
     }
 
 }
