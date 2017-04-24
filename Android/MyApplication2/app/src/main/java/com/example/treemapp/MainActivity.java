6b0ce3404f973657305efb74e3fc365e3a356488
Tino Schulz <tinu@Tinos-MacBook-Pro.local>
2017-04-24 13:18:23 +0200

    again

--- Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
+++ Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.treemapp;
 
+import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
-
+import static com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView.ORIENTATION_0;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private SubsamplingScaleImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Setting the image to display
-        SubsamplingScaleImageView imageView = (SubsamplingScaleImageView) findViewById(R.id.imageView);
+        imageView = (SubsamplingScaleImageView) findViewById(R.id.imageView);
         imageView.setImage(ImageSource.resource(R.drawable.tree));
         //imageView.setImage(ImageSource.uri("/sdcard/DCIM/DSCM00123.JPG"));
+
+        // Display image in its native orientation
+        imageView.setOrientation(ORIENTATION_0);
+
+        final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent e) {
+                Toast error = Toast.makeText(MainActivity.this, "Error 1!", Toast.LENGTH_SHORT);
+                error.show();
+
+                if (imageView.isReady()) {
+                    PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                    // ...
+
+                    Toast errorToast = Toast.makeText(MainActivity.this, "Error, pls chech your internet connection and try again!", Toast.LENGTH_SHORT);
+                    errorToast.show();
+                }
+                return true;
+            }
+        });
     }
+
 
 }
