371e345a452cef7a89e9b0c1a8cef711a24f0361
5dv115 <5dv115@oin.dklab.cs.umu.se>
2017-04-25 09:42:27 +0200

    c++project

---
+++ c++Project/CMakeFiles/c__Project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c__Project.dir/main.cpp.o"
+  "CMakeFiles/c__Project.dir/TestClass.cpp.o"
+  "c__Project.pdb"
+  "c__Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c__Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
