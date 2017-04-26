8bd57263374758196b66663afe68512020aae28b
Tino Schulz <tinu@Tinos-MacBook-Pro.local>
2017-04-26 11:40:30 +0200

    m

--- Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
+++ Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
@@ -53,9 +53,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         // Setting the image to display
         imageView = (PinView) findViewById(R.id.imageView);
-        imageView.setImage(ImageSource.resource(R.drawable.tree));
+        //imageView.setImage(ImageSource.resource(R.drawable.tree));
+        imageView.setImage(ImageSource.uri("/Documents/GitHub/Computer-Vision/Android/Images"));
         imageView.setMaxScale(10f);
-
 
 
 
