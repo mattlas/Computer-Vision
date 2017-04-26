724c80683f847ab84fe65909e5865ca3bbe1a473
Shanmuganathan Sabarisathasivam <raghul.shanmuganathan06@gmail.com>
2017-04-26 16:22:29 +0200

    Added readimage, displayimage functions

--- c++Project/src/Main.cpp
+++ c++Project/src/Main.cpp
@@ -6,13 +6,57 @@
 // Description : Method for positioning field measured trees relative to image data for building a 3D model.
 //============================================================================
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
 #include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+Mat readimage(String imagename)
+{
+    Mat image;
+    image = imread( imagename, IMREAD_COLOR );          // Read the Image file
+    if( image.empty() )                         
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+    }
+    return(image);
+    
+}
+    
+void displayimage(Mat img)
+{
+    namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a new window to display the image
+    imshow( "Display window", img );                // Display the image
+    waitKey(0); // Wait for a keystroke in the window
+}
 
 extern "C"{
 #include <vl/generic.h>
 }
 
-int main(){
-    VL_PRINT ("vlfeat loaded properly\n");
-    return 0;
+int main( int argc, char** argv )
+{
+    String imageName;
+    Mat image;
+    try
+    {
+        if( argc > 1)
+        {
+            imageName = argv[1];
+        }
+    }
+    catch(...)
+    {
+        cout<<"Please specify a Image filename to read"<< std::endl;
+    }
+    
+    image=readimage(imageName);
+    displayimage(image);
 }
+        
+    
