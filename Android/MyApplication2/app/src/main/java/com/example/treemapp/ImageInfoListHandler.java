a5e60870e848eba79e38aaafe5fb1e02662b94ff
oskarOlausson <kullenoskar@gmail.com>
2017-04-27 16:35:37 +0200

    Loading from SD card now works
    Both the mosaic image and the image list,
    also made a folder on the android tablet with a reasonable structure.

--- Android/MyApplication2/app/src/main/java/com/example/treemapp/ImageInfoListHandler.java
+++ Android/MyApplication2/app/src/main/java/com/example/treemapp/ImageInfoListHandler.java
@@ -1,21 +1,22 @@
 package com.example.treemapp;
 
-import android.media.Image;
-import android.media.ImageReader;
+import android.app.Activity;
+import android.os.Environment;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
-import android.widget.ImageView;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by oskar on 2017-04-26.
@@ -24,13 +25,25 @@ import java.util.Map;
 public class ImageInfoListHandler {
 
     private final String TAG = ImageInfoListHandler.class.getSimpleName();
-    private String imageFolderName = "/sdcard/mosaic/mosaicImage/";
+    private String folderName = Environment.getExternalStorageDirectory() + "/mosaic/";
+    private String imageFolderName = folderName + "images/";
 
     private HashMap<String, ImageInfo> imageInfos;
 
     public ImageInfoListHandler() {
-
+        String fileName = "imageList.csv";
+        File file = new File(folderName + fileName);
+        FileReader fileReader;
         imageInfos = new HashMap<>();
+
+        try {
+            fileReader = new FileReader(file);
+            parseFileToHashMap(fileReader);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "Could not find file: '"+ fileName + "' in folder '" + folderName + "'");
+        }
+
+        Log.d(TAG, "loaded " + imageInfos.size() + " numbers of lines from imageList.csv");
     }
 
     public void parseFileToHashMap(BufferedReader bf) {
@@ -43,7 +56,7 @@ public class ImageInfoListHandler {
 
         try {
             while((line = bf.readLine()) != null) {
-
+                line = line.replaceAll("\"", "");
                 words = line.split(",");
                 imageInfo = new ImageInfo(words);
                 imageInfos.put(imageInfo.getFileName(), imageInfo);
@@ -66,8 +79,33 @@ public class ImageInfoListHandler {
         return imageInfos.get(fileName);
     }
 
-    public String loadImage(ImageInfo im) {
-        return imageFolderName + im.getFileName();
+    /**
+     * Returns image files located in mosaic/images, used for viewing a point from different angles
+     * @param fileName no path, just the fileName
+     * @return an imagesource which can be used to set in an image view
+     */
+    public ImageSource loadImage(String fileName) {
+        String location = imageFolderName + fileName;
+        return ImageSource.uri(location);
+    }
+
+    public ImageSource loadImage(ImageInfo im) {
+        return loadImage(im.getFileName());
+    }
+
+    /*These two returns all neighbors to that image as ImageSources*/
+    public List<ImageSource> loadNeighboringImages(ImageInfo im) {
+        List<ImageSource> neighbors = new ArrayList<>();
+
+        for (String neighborName: im.getNeighbors()) {
+            neighbors.add(loadImage(neighborName));
+        }
+
+        return neighbors;
+    }
+
+    public List<ImageSource> loadNeighboringImages(String fileName) {
+        return loadNeighboringImages(imageInfos.get(fileName));
     }
 
     public ImageInfo findImageClosestTo(int x, int y) {
