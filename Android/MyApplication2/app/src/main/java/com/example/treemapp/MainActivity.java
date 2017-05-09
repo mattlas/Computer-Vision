023d42bdc7a0458527db8cc690df9c9d87f2a404
Tino Schulz <tinu@Tinos-MacBook-Pro.local>
2017-05-09 16:47:15 +0200

    input style changed

--- Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
+++ Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
@@ -333,9 +333,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         final Spinner species = (Spinner) findViewById(R.id.inp_species);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.trees_array, android.R.layout.simple_spinner_item);
+                R.array.trees_array, R.layout.spinner_item);
         // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
         // Apply the adapter to the spinner
 
         species.setAdapter(adapter);
