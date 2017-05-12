aa04514f11bec2465b7af6a092d8d3fa77325100
rathdrummer <tfy09dnn@cs.umu.se>
2017-05-12 10:26:33 +0200

    indentation

--- c++Project/spike/exif/exif.h
+++ c++Project/spike/exif/exif.h
@@ -14,13 +14,13 @@ public:
 	std::string ImageDescription;     	// Image description
 	std::string Make;                	// Camera manufacturer's name
 	std::string Model;               	// Camera model
-	unsigned short Orientation;			// Image orientation, start of data corresponds to
-										// 0: unspecified in EXIF data
-										// 1: upper left of image
-										// 3: lower right of image
-										// 6: upper right of image
-										// 8: lower left of image
-										// 9: undefined
+	unsigned short Orientation;		// Image orientation, start of data corresponds to
+						// 0: unspecified in EXIF data
+						// 1: upper left of image
+						// 3: lower right of image
+						// 6: upper right of image
+						// 8: lower left of image
+						// 9: undefined
 	unsigned short BitsPerSample;     	// Number of bits per component
 	std::string Software;           	// Software used
 	std::string DateTime;           	// File change date and time
@@ -31,46 +31,46 @@ public:
 	double ExposureTime;              	// Exposure time in seconds
 	double FNumber;                   	// F/stop
 	unsigned short ExposureProgram;		// Exposure program
-										// 0: Not defined
-										// 1: Manual
-										// 2: Normal program
-										// 3: Aperture priority
-										// 4: Shutter priority
-										// 5: Creative program
-										// 6: Action program
-										// 7: Portrait mode
-										// 8: Landscape mode
+						// 0: Not defined
+						// 1: Manual
+						// 2: Normal program
+						// 3: Aperture priority
+						// 4: Shutter priority
+						// 5: Creative program
+						// 6: Action program
+						// 7: Portrait mode
+						// 8: Landscape mode
 	unsigned short ISOSpeedRatings;		// ISO speed
-	double ShutterSpeedValue;			// Shutter speed (reciprocal of exposure time)
-	double ExposureBiasValue;			// Exposure bias value in EV
-	double SubjectDistance;				// Distance to focus point in meters
-	double FocalLength;					// Focal length of lens in millimeters
+	double ShutterSpeedValue;		// Shutter speed (reciprocal of exposure time)
+	double ExposureBiasValue;		// Exposure bias value in EV
+	double SubjectDistance;			// Distance to focus point in meters
+	double FocalLength;			// Focal length of lens in millimeters
 	unsigned short FocalLengthIn35mm;	// Focal length in 35mm film
-	char Flash;         				// 0 = no flash, 1 = flash used
+	char Flash;         			// 0 = no flash, 1 = flash used
 	unsigned short FlashReturnedLight;	// Flash returned light status
-										// 0: No strobe return detection function
-										// 1: Reserved
-										// 2: Strobe return light not detected
-										// 3: Strobe return light detected
-	unsigned short FlashMode;			// Flash mode
-										// 0: Unknown
-										// 1: Compulsory flash firing
-										// 2: Compulsory flash suppression
-										// 3: Automatic mode
+						// 0: No strobe return detection function
+						// 1: Reserved
+						// 2: Strobe return light not detected
+						// 3: Strobe return light detected
+	unsigned short FlashMode;		// Flash mode
+						// 0: Unknown
+						// 1: Compulsory flash firing
+						// 2: Compulsory flash suppression
+						// 3: Automatic mode
 	unsigned short MeteringMode;		// Metering mode
-										// 1: average
-										// 2: center weighted average
-										// 3: spot
-										// 4: multi-spot
-										// 5: multi-segment
-	unsigned ImageWidth;				// Image width reported in EXIF data
-	unsigned ImageHeight;				// Image height reported in EXIF data
-	struct Geolocation_t{				// GPS information embedded in file
-		double Latitude;				// Image latitude expressed as decimal
-		double Longitude;				// Image longitude expressed as decimal
-		double Altitude;      		    // Altitude in meters, relative to sea level
-		char AltitudeRef;				// 0 = above sea level, -1 = below sea level
-		double DOP;						// GPS degree of precision (DOP)
+						// 1: average
+						// 2: center weighted average
+						// 3: spot
+						// 4: multi-spot
+						// 5: multi-segment
+	unsigned ImageWidth;			// Image width reported in EXIF data
+	unsigned ImageHeight;			// Image height reported in EXIF data
+	struct Geolocation_t{			// GPS information embedded in file
+		double Latitude;		// Image latitude expressed as decimal
+		double Longitude;		// Image longitude expressed as decimal
+		double Altitude;      		// Altitude in meters, relative to sea level
+		char AltitudeRef;		// 0 = above sea level, -1 = below sea level
+		double DOP;			// GPS degree of precision (DOP)
 		struct Coord_t{
 			double degrees;
 			double minutes;
@@ -78,7 +78,7 @@ public:
 			char direction;
 		} LatComponents, LonComponents;	// Latitude, Longitude expressed in deg/min/sec
 	} GeoLocation;
-	struct LensInfo_t{					// Lens information
+	struct LensInfo_t{			// Lens information
 		double FStopMin;                // Min aperture (f-stop)
 		double FStopMax;                // Max aperture (f-stop)
 		double FocalLengthMin;          // Min focal length (mm)
