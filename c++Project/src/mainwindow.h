123a5d8cda4f5468332e7e6811ed93f992e0904a
Raghul Shanmuganathan <raghul.shanmuganathan06@gmail.com>
2017-05-17 13:52:45 +0200

    update mainwindow.h

--- c++Project/src/mainwindow.h
+++ c++Project/src/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <string>
+#include <QString>
+
 
 namespace Ui {
 class MainWindow;
@@ -11,9 +14,17 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+
+    bool saveImages;
+    std::string path;
+
+
 public:
     explicit MainWindow(QWidget *parent = 0);
+    bool saveImagesOnPC(bool SAVE);
+    std::string QstringToString(QString value);
     ~MainWindow();
+    QString qpath;
 
 private slots:
     void on_button_browse_clicked();
@@ -21,6 +32,14 @@ private slots:
     void on_button_next_clicked();
 
     void on_button_create_clicked();
+
+    void on_button_next_4_clicked();
+
+    void on_label_linkActivated(const QString &link);
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
 
 private:
     Ui::MainWindow *ui;
