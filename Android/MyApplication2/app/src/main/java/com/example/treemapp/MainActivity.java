66340b4fd5b86357943eee127f0172fd5d28965a
oskarOlausson <kullenoskar@gmail.com>
2017-05-17 16:38:52 +0200

    side menu exists, is not linked to anything yet

--- Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
+++ Android/MyApplication2/app/src/main/java/com/example/treemapp/MainActivity.java
@@ -11,12 +11,19 @@ import android.os.Vibrator;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.graphics.PointF;
 import android.view.GestureDetector;
@@ -40,7 +47,7 @@ import in.goodiebag.carouselpicker.CarouselPicker;
 import static com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView.ORIENTATION_0;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     private Overlay overlay;
     private Vibrator vibrator;
@@ -92,55 +99,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     private void initMenu() {
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        // Set the adapter for the list view
-        /*
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
-        */
-        //TODO, fix so mdrawerlist is not null
-        if (mDrawerList != null) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-            // Set the list's click listener
-            mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-            mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                    R.string.open_menu, R.string.close_menu) {
-
-                /**
-                 * Called when a drawer has settled in a completely closed state.
-                 */
-                public void onDrawerClosed(View view) {
-                    super.onDrawerClosed(view);
-                    getActionBar().setTitle("Closing");
-                    invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-                }
-
-                /**
-                 * Called when a drawer has settled in a completely open state.
-                 */
-                public void onDrawerOpened(View drawerView) {
-                    super.onDrawerOpened(drawerView);
-                    getActionBar().setTitle("welcome!");
-                    invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-                }
-            };
-
-            // Set the drawer toggle as the DrawerListener
-            mDrawerLayout.addDrawerListener(mDrawerToggle);
-        }
-    }
-
-    /* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        //to things
-        //menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void imageViewSetUp() {
@@ -359,6 +329,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     public Overlay getOverlay() {
         return overlay;
+    }
+
+    //navigationbar stuff
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 
     private class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
