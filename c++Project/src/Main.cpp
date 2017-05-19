6535b88b163a480a9db57e7fb4affc340521996a
5dv115 <5dv115@ori.dklab.cs.umu.se>
2017-05-19 11:05:08 +0200

    jonas comment out main

--- c++Project/src/Main.cpp
+++ c++Project/src/Main.cpp
@@ -16,7 +16,6 @@
 
 // Qt
 #include "mainwindow.h"
-//#include <QtGui>
 #include <QApplication>
 #include <QMainWindow>
 
@@ -33,7 +32,6 @@ int main(int argc, char **argv){
     MainWindow w;
     w.show();
 
-
 //    const char *ip_path;
 //    if(argc > 1){
 //    	ip_path = argv[1];
@@ -44,20 +42,21 @@ int main(int argc, char **argv){
 //        ip_path=(std::string)MainWindow::qpath;
 //    }
 //
-    std::string op_path;
+    /*std::string op_path;
 //    if(argc > 1){
 //        op_path = argv[2];
 //
 //    } else {
         //op_path = "/home/5dv115/Computer-Vision/c++Project/PGMdir";
 //        op_path = "/home/5dv115/c13evk_scripts/pict_out";
+
+    if(w.wait_path == 1)
+    {
         op_path=w.QstringToString(w.qpath);
-    std::cout << "String =" << op_path << std::endl;
+        std::cout << "String =" << op_path << std::endl;
         //    }
-
-
-    time_t start = time(0);
-
+        time_t start = time(0);
+    }
     // Feature points extraction
 //    MosaicData *data = new MosaicData();
 
@@ -72,6 +71,6 @@ int main(int argc, char **argv){
     //time_t end = time(0);
 //    double time = difftime(end, start) * 1000.0;
     std::cout << "time = " << time << std::endl;
-//    return 0;
+//    return 0;*/
     return app.exec();
 }
