34f6494c7a32e540738a7e8d4ffbab1fc6556f21
oskarOlausson <kullenoskar@gmail.com>
2017-05-22 11:03:38 +0200

    small change

--- Android/MyApplication2/app/src/main/java/com/example/treemapp/OriginalGestureDetector.java
+++ Android/MyApplication2/app/src/main/java/com/example/treemapp/OriginalGestureDetector.java
@@ -1,5 +1,6 @@
 package com.example.treemapp;
 
+import android.graphics.PointF;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.Toast;
@@ -11,42 +12,69 @@ import android.widget.Toast;
 public class OriginalGestureDetector extends GestureDetector.SimpleOnGestureListener {
 
     private OnePinView opw;
+    private boolean drag;
 
-    public OriginalGestureDetector(OnePinView main) {
-        this.opw = main;
+    public OriginalGestureDetector(OnePinView onePinView) {
+        this.opw = onePinView;
+        drag = false;
+    }
+
+    public boolean getDrag() {
+        return drag;
+    }
+
+    public void turnOffDrag() {
+        drag = false;
+    }
+
+    public void setUpDragPin(MotionEvent e) {
+        Pin pin = opw.getPin();
+
+        if (pin != null) {
+            if (opw.euclidanViewDistance(pin, e.getX(), e.getY()) < pin.getCollisionRadius()) {
+
+                pin.setDragged(true);
+                opw.setZoomEnabled(false);
+
+                /* When you set panEnabled to false, Dave Morrisey (who wrote the image view code).
+                * decided that you want to center the image aswell, so we will transform it back */
+                float scale = opw.getScale();
+                PointF center = opw.getCenter();
+
+                opw.setPanEnabled(false);
+                opw.setScaleAndCenter(scale, center);
+
+                opw.invalidate();
+            }
+            else {
+                drag = false;
+            }
+        }
+        else drag = false;
     }
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent pos) {
         //viewSwitcher.showNext();
-        if (opw..isReady()) {
+        if (opw.isReady()) {
             // If there is no pins we are definitely creating a new one
             Pin pin = opw.getPin();
 
             if (pin != null) {
-
                 // If tabbed position is inside collision radius of a pin -> edit this pin
                 if (opw.euclidanViewDistance(pin, pos.getX(), pos.getY()) < pin.getCollisionRadius()){
                     opw.invalidate();
-                    // otherwise make new pin
-                } else {
-                    //If the user's presses not near an existing pin we make a new one
-                    opw.showOriginals(pos);
+                    drag = true;
+                    setUpDragPin(pos);
                 }
             }
 
-        } else {
-            Toast.makeText(opw.getApplicationContext(), "Single tap: Image not ready", Toast.LENGTH_SHORT).show();
         }
         return true;
     }
     @Override
     public void onLongPress(MotionEvent e) {
-        if (opw.getImageView().isReady()) {
-            opw.setUpDragPin(e);
-        } else {
-            Toast.makeText(opw.getApplicationContext(), "Long press: Image not ready", Toast.LENGTH_SHORT).show();
-        }
+        //nothing now
     }
 
     @Override /*This zooms in and out*/
